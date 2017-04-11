8b61738d646b3e4600ba1911b4136cf61f62135e
Hal Smith Stevens <hal@pixelfusion.co.nz>
2017-04-11 16:39:53 +1200

    ...

--- app/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Home extends React.Component {
-    render() {
-        return (<div><h1>Home Home Home...</h1></div>);
-    }
-}
-
-export default Home;
--- app/pages/error/FourOhFour.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class FourOhFour extends React.Component {
-    render() {
-        return (<div><h1>404</h1></div>);
-    }
-}
-
-export default FourOhFour;
