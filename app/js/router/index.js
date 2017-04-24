69d596018d85a54884da296b4e74ac4224e85115
hally9k <hally9k@gmail.com>
2017-04-24 16:00:56 +1200

    ...

--- app/js/router/index.js
+++ app/js/router/index.js
@@ -1,10 +1,7 @@
-import Ping from '../modules/root/ping/ping-container';
+import ContentPage from '../pages/content-page.container';
 import React from 'react';
-import Root from '../modules/root/root-container';
 import { Route } from 'react-router';
 
 export default <div>
-                    <Route path="/" exact render={ () => <h1>Root</h1> }></Route>
-                    <Route path="/github" component={Root}></Route>
-                    <Route path="/ping" component={Ping}></Route>
-                </div>;
+                   <Route path="*" component={ContentPage} />
+               </div>;
